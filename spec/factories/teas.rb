3781baad83378cef581de1fa0fef39f6922c0dac
Laura Court <laura.mcourt@gmail.com>
2022-01-07 06:32:00 -0500

    Add factories

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tea do
-    title { "MyString" }
-    description { "MyString" }
-    temperature { 1 }
-    brew_time { 1 }
+    title { Faker::JapaneseMedia::StudioGhibli.movie }
+    description { Faker::JapaneseMedia::StudioGhibli.quote }
+    temperature { Faker::Number.within(range: 100..150) }
+    brew_time { Faker::Number.within(range: 1..5) }
   end
 end
